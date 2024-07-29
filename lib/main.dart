89b304dfbccab2d6f59d4639d897a914b7a25648
Vladimir <vladdigen@gmail.com>
2024-07-29 23:11:41 +0400

    Showing images and using buttons

--- lib/main.dart
+++ lib/main.dart
@@ -24,10 +24,17 @@ class MyApp extends StatelessWidget {
               ),
               Text(
                 'Discover the World',
-                style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.deepPurpleAccent),
+                style:
+                    TextStyle(fontSize: 20.0, color: Colors.deepPurpleAccent),
               ),
+              Image.network(
+                'https://images.freeimages.com/images/large-previews/eaa/the-beach-1464354.jpg',
+                height: 350,
+              ),
+              ElevatedButton(
+                onPressed: () => true,
+                child: Text('Contact Us'),
+              )
             ],
           ),
         ),
